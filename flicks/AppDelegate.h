79a7bee29057390036aabc58886ab45c33c457d5
Amie Kweon <amie.kweon@rd.io>
2014-06-09 00:19:53 -0700

    Add project files

---
+++ flicks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  flicks
+//
+//  Created by Amie Kweon on 6/9/14.
+//  Copyright (c) 2014 Amie Kweon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ flicks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  flicks
+//
+//  Created by Amie Kweon on 6/9/14.
+//  Copyright (c) 2014 Amie Kweon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
