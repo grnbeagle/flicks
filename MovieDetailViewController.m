9fde9edf7c5f9341f04897332dd3c7f84adf76a2
Amie Kweon <amie.kweon@rd.io>
2014-06-10 04:03:15 -0700

    Remove toolbar code from detail controller

--- MovieDetailViewController.m
+++ MovieDetailViewController.m
@@ -66,57 +66,12 @@
     
     float scrollHeight = offset + contentView.frame.size.height - self.navigationController.toolbar.frame.size.height;
     [self.scrollView setContentSize: CGSizeMake(320, scrollHeight)];
-    
-    [self createToolbar];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (void)createToolbar {
-    UIToolbar *toolbar = [[UIToolbar alloc] init];
-    [toolbar setTintColor:[UIColor blackColor]];
-    [toolbar setBarTintColor:[UIColor redColor]];
-    toolbar.frame = CGRectMake(0, 0, 300, 44);
-    UIBarButtonItem *flexiableItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
-    
-    UIImage *movieIconImage = [UIImage imageNamed:@"MovieIcon"];
-    UIButton *movieButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    [movieButton setImage:movieIconImage forState:UIControlStateNormal];
-    [movieButton setTitle:@"Movies" forState:UIControlStateNormal];
-    [movieButton setImageEdgeInsets:UIEdgeInsetsMake(0, 0, 0, 10)];
-    [movieButton setTitleEdgeInsets:UIEdgeInsetsMake(3, 10, 0, 0)];
-    movieButton.titleLabel.font = [UIFont systemFontOfSize:12];
-    movieButton.frame = CGRectMake(0, 0, 90, 30);
-    
-    [movieButton addTarget:self action:@selector(viewListScreen:) forControlEvents:UIControlEventTouchUpInside];
-    
-    UIBarButtonItem *movieBtn = [[UIBarButtonItem alloc] initWithCustomView:movieButton];
-    
-    UIImage *dvdIconImage = [UIImage imageNamed:@"DVDIcon"];
-    UIButton *dvdButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    [dvdButton setImage:dvdIconImage forState:UIControlStateNormal];
-    [dvdButton setTitle:@"DVD" forState:UIControlStateNormal];
-    [dvdButton setImageEdgeInsets:UIEdgeInsetsMake(0, 0, 0, 10)];
-    [dvdButton setTitleEdgeInsets:UIEdgeInsetsMake(3, 10, 0, 0)];
-    dvdButton.titleLabel.font = [UIFont systemFontOfSize:12];
-    dvdButton.frame = CGRectMake(170, 0, 90, 30);
-    
-    [dvdButton addTarget:self action:@selector(viewListScreen:) forControlEvents:UIControlEventTouchUpInside];
-    UIBarButtonItem *dvdBtn = [[UIBarButtonItem alloc] initWithCustomView:dvdButton];
-    
-    NSArray *buttons = [NSArray arrayWithObjects:movieBtn, flexiableItem, dvdBtn, nil];
-    self.toolbarItems = buttons;
-}
-
-- (void)viewListScreen:(id)sender  {
-    NSString *listType = [[(UIButton *)sender titleLabel] text];
-    MoviesViewController *moviesViewController = [[MoviesViewController alloc] init];
-    moviesViewController.listType = listType;
-    [self.navigationController popToViewController:moviesViewController animated:YES];
 }
 
 @end
